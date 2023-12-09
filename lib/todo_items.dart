cddf7103f14641d4ea3412734f574e018673adf2
ImanAdithya <adithyaiman07@gmail.com>
2023-12-09 18:49:59 +0530

    content updated

--- lib/todo_items.dart
+++ lib/todo_items.dart
@@ -5,6 +5,31 @@ class ToDOItems extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: ListTile(
+        onTap: (){},
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+        tileColor: Colors.white,
+        leading: Icon(Icons.check_box,color: Colors.deepPurple,),
+        title: Text(
+          "Check Main",
+          style: TextStyle(
+            fontSize: 16,
+            decoration: TextDecoration.lineThrough
+          ),
+        ),
+        trailing: Container(
+          width: 35,
+          height: 35,
+          decoration: BoxDecoration(
+            color: Colors.red,
+            borderRadius: BorderRadius.circular(5),
+          ),
+          child: Icon(Icons.delete,color: Colors.white,),
+        ),
+      ),
+    );
   }
 }
